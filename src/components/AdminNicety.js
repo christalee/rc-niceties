d3854df704326c71cd2d9a93e51d3301b3509add
Christalee Bieber <cbieber@alum.mit.edu>
2021-03-16 16:22:09 -0400

    added faculty_reviewed checkbox

--- src/components/AdminNicety.js
+++ src/components/AdminNicety.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Checkbox } from 'react-bootstrap';
+import { Checkbox } from 'react-bootstrap';
 import $ from 'jquery';
 
 import SaveButton from './SaveButton';
@@ -8,15 +8,19 @@ const AdminNicety = React.createClass({
     getInitialState: function() {
         return {
             text: this.props.nicety.text,
-            noSave: true
+            noSave: true,
+            reviewedValue: this.props.nicety.reviewed.toString()
         };
     },
-
+    reviewedChange: function(event) {
+      this.setState({reviewedValue: event.target.checked.toString(), noSave: false});
+    },
     saveNicety: function() {
         const data = {
             text: this.state.text,
             author_id: this.props.nicety.author_id,
-            target_id: this.props.target_id
+            target_id: this.props.target_id,
+            faculty_reviewed: this.state.reviewedValue
         }
         $.ajax({
             url: this.props.admin_edit_api,
@@ -38,42 +42,53 @@ const AdminNicety = React.createClass({
     },
 
     render: function() {
-        let nicetyName;
-        if ('name' in this.props.nicety) {
-            nicetyName = this.props.nicety.name;
-        } else  {
-            nicetyName = 'Anonymous';
-        }
+      let reviewedRender;
+      if (this.state.reviewedValue === "true") {
+        reviewedRender = (<Checkbox
+          checked="checked"
+          onChange={this.reviewedChange}>Reviewed</Checkbox>);
+      } else if (this.state.reviewedValue === "false") {
+        reviewedRender = (<Checkbox
+          onChange={this.reviewedChange}>Reviewed</Checkbox>);
+      }
 
-        let noRead = null;
-        if (this.props.nicety.no_read === true) {
-            noRead = "Don't Read At Ceremony, Please";
-        }
+      let nicetyName;
+      if ('name' in this.props.nicety) {
+          nicetyName = this.props.nicety.name;
+      } else  {
+          nicetyName = 'Anonymous';
+      }
 
-        let nicetyReturn = null;
-        if (this.props.nicety.text !== '' && this.props.nicety.text !== null) {
-            const textStyle = {
-                width: '75%'
-            }
-            nicetyReturn = (
-                <div>
-                    <h4>From {nicetyName}</h4>
-                    <h5>{noRead}</h5>
-                    <textarea
-                        defaultValue={this.state.text}
-                        onChange={this.textareaChange}
-                        rows="6"
-                        style={textStyle} />
-                     <SaveButton
-                        noSave={this.state.noSave}
-                        onClick={this.saveNicety}>
-                        Save
-                    </SaveButton>
-                    <br />
-                </div>
-            );
-        }
-        return nicetyReturn;
+      let noRead = null;
+      if (this.props.nicety.no_read === true) {
+          noRead = "Don't Read At Ceremony, Please";
+      }
+
+      let nicetyReturn = null;
+      if (this.props.nicety.text !== '' && this.props.nicety.text !== null) {
+          const textStyle = {
+              width: '75%'
+          }
+          nicetyReturn = (
+              <div>
+                <h4>From {nicetyName}</h4>
+                <h5>{noRead}</h5>
+                <textarea
+                  defaultValue={this.state.text}
+                  onChange={this.textareaChange}
+                  rows="6"
+                  style={textStyle} />
+                <SaveButton
+                  noSave={this.state.noSave}
+                  onClick={this.saveNicety}>
+                  Save
+                </SaveButton>
+                {reviewedRender}
+                <br />
+              </div>
+          );
+      }
+      return nicetyReturn;
     }
 });
 
